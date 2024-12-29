39f90b6b7ab3edf93018aa2a9c95459cdf91b035
Amin <Amin@DESKTOP-VTEG5B0>
2024-12-29 22:42:37 +0330

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnPredict = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(258, 71);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(297, 68);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnPredict
+            // 
+            this.btnPredict.Location = new System.Drawing.Point(369, 169);
+            this.btnPredict.Name = "btnPredict";
+            this.btnPredict.Size = new System.Drawing.Size(75, 23);
+            this.btnPredict.TabIndex = 2;
+            this.btnPredict.Text = "button1";
+            this.btnPredict.UseVisualStyleBackColor = true;
+            this.btnPredict.Click += new System.EventHandler(this.btnPredict_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Vazir", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(248, 213);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(316, 38);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPredict);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnPredict;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private MLContext mlContext;
+        private PredictionEngine<SentimentData, SentimentPrediction> predictionEngine;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMLModel();
+
+        }
+        private void InitializeMLModel()
+        {
+           
+            mlContext = new MLContext();
+
+          
+            var data = mlContext.Data.LoadFromTextFile<SentimentData>(
+                path: @"sentiment_data.csv",
+                hasHeader: true,
+                separatorChar: ',',
+                allowQuoting: true);
+
+            var pipeline = mlContext.Transforms.Text.FeaturizeText("Features", "Text")
+                          .Append(mlContext.Transforms.Conversion.MapValueToKey("Label", "Sentiment"))
+                          .Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy())
+                          .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            var tts = mlContext.Data.TrainTestSplit(data, testFraction: 0.2);
+            var model = pipeline.Fit(tts.TrainSet);
+         
+            
+            predictionEngine = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+            
+        }
+
+        private void btnPredict_Click(object sender, EventArgs e)
+        {
+            var inputText = txtInput.Text;
+
+            if (inputText.Length==0)
+            {
+                lblResult.Text = "لطفاً یک متن وارد کنید.";
+                return;
+            }
+
+            var input = new SentimentData { Text = inputText };
+            var prediction = predictionEngine.Predict(input);
+            switch (prediction.PredictedLabel)
+            {
+                case "Positive":
+                    lblResult.Text = "بنظر میرسه این متن حس خوبی داره ! ";
+                    break;
+                case "Negative":
+                    lblResult.Text = "اوه اوه ... مث اینکه یکی اینجا ناراحته ";
+                    break;
+                case "Neutral":
+                    lblResult.Text = "بی فایده س ! این متن هیچ حسی نداره  :(( ";
+                    break ;
+                default:
+                    lblResult.Text = "اگه مطمئنی که این متن حاوی احساسه باید بیشتر روی دیتاستم کار کنی";
+                    break;
+            }
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class SentimentData
+        {
+            [LoadColumn(0)]
+            public string Text { get; set; }
+
+            [LoadColumn(1)]
+            public string Sentiment { get; set; }
+        }
+
+        // Output class
+        public class SentimentPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public string PredictedLabel { get; set; }
+        }
+    }
+}
